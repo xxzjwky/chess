e5f82fbfac04e906a4875a1bdbc2e02ba08492a3
Tedu <373581673@qq.com>
2019-04-26 20:26:16 +0800

    连点子bug修复

--- chess.py
+++ chess.py
@@ -4,7 +4,7 @@ import tkinter.font as tkFont
 
 
 #棋盘列表
-list_chess = [
+list_position = [
     [(62,25), (140,25), (220,25), (300,25), (379,25), (459,25), (538,25), (617,25), (695,25)],
     [(62,100),(140,100),(220,100),(300,100),(379,100),(459,100),(538,100),(617,100),(695,100)],
     [(62,180),(140,180),(220,180),(300,180),(379,180),(459,180),(538,180),(617,180),(695,180)],
@@ -17,6 +17,10 @@ list_chess = [
     [(62,740),(140,740),(220,740),(300,740),(379,740),(459,740),(538,740),(617,740),(695,740)]
 ]
 
+#象棋列表
+list_chess = []
+
+#当前点击的棋子
 click_item = {}
 
 
@@ -29,15 +33,15 @@ class Chess:
         :param color: 棋子颜色
         :param position: 棋子位置
         """
-
-
         self.color = color
-
+        self.position = position
 
         ft = tkFont.Font(family='微软雅黑', size=20, weight=tkFont.BOLD)
         self.btn = Button(root,text=name, bg="#d1b07e",fg=color, font=ft,height = "1",width = "1", command=self.click)
         self.btn.pack()
         self.btn.place(x=position[0], y=position[1])
+
+        list_chess.append(self)
     def click(self):
         #点击变黄色背景　并且标记
         self.btn['bg']="yellow"
@@ -46,6 +50,13 @@ class Chess:
         if click_item:
             click_item["data"].btn['bg'] = "#d1b07e"
         click_item["data"]= self
+
+    def delete(self):
+        """
+        删除控件
+        :return:
+        """
+        self.btn.place_forget()
 
 
 root = Tk(className = "中国象棋")
@@ -57,10 +68,16 @@ def find_move_location(click_location):
     :param click_location:
     :return:
     """
-    for i in range(len(list_chess)):
-        for j in range(len(list_chess[i])):
-            chess_one = list_chess[i][j]
+    for i in range(len(list_position)):
+        for j in range(len(list_position[i])):
+            chess_one = list_position[i][j]
             if abs(chess_one[0] - click_location[0])<40 and abs(chess_one[1] - click_location[1])<40:
+
+                #删除该点的子
+                for item in list_chess:
+                    if chess_one == item.position:
+                        item.delete()
+
                 return chess_one
 
 
@@ -72,8 +89,10 @@ def showlocation(event):
     :param event:
     :return:
     """
-    print(event.x,event.y)
-    print(click_item)
+
+    # print(list_chess)
+    # print(click_item)
+    # print(event.x,event.y)
     if click_item:
         #移动棋子
         chess = click_item["data"]
@@ -95,133 +114,133 @@ label_img.bind("<Button-1>", showlocation)
 label_img.pack()
 
 #左黑车
-pos_black_left_rook = list_chess[0][0]
+pos_black_left_rook = list_position[0][0]
 black_left_rook = Chess(root,"車","black",pos_black_left_rook)
 
 #左黑马
-pos_black_left_knight = list_chess[0][1]
+pos_black_left_knight = list_position[0][1]
 black_left_knight = Chess(root,"馬","black",pos_black_left_knight)
 
 #左黑象
-pos_black_left_minister = list_chess[0][2]
+pos_black_left_minister = list_position[0][2]
 black_left_minister = Chess(root,"象","black",pos_black_left_minister)
 
 #左黑士
-pos_black_left_guard = list_chess[0][3]
+pos_black_left_guard = list_position[0][3]
 black_left_guard = Chess(root,"士","black",pos_black_left_guard)
 
 #黑将
-pos_black_king = list_chess[0][4]
+pos_black_king = list_position[0][4]
 black_king = Chess(root,"将","black",pos_black_king)
 
 #右黑士
-pos_black_right_guard = list_chess[0][5]
+pos_black_right_guard = list_position[0][5]
 black_right_guard = Chess(root,"士","black",pos_black_right_guard)
 
 #右黑象
-pos_black_right_minister = list_chess[0][6]
+pos_black_right_minister = list_position[0][6]
 black_right_minister = Chess(root,"象","black",pos_black_right_minister)
 
 #右黑马
-pos_black_right_knight = list_chess[0][7]
+pos_black_right_knight = list_position[0][7]
 black_right_knight = Chess(root,"馬","black",pos_black_right_knight)
 
 #右黑车
-pos_black_right_rook = list_chess[0][8]
+pos_black_right_rook = list_position[0][8]
 black_right_rook = Chess(root,"車","black",pos_black_right_rook)
 
 #左黑炮
-pos_black_left_gun = list_chess[2][1]
+pos_black_left_gun = list_position[2][1]
 black_left_gun = Chess(root,"炮","black",pos_black_left_gun)
 
 #右黑炮
-pos_black_right_gun = list_chess[2][7]
+pos_black_right_gun = list_position[2][7]
 black_right_gun = Chess(root,"炮","black",pos_black_right_gun)
 
 #黑卒１
-pos_black_pawn1 = list_chess[3][0]
+pos_black_pawn1 = list_position[3][0]
 black_black_pawn1 = Chess(root,"卒","black",pos_black_pawn1)
 
 #黑卒2
-pos_black_pawn2 = list_chess[3][2]
+pos_black_pawn2 = list_position[3][2]
 black_black_pawn2 = Chess(root,"卒","black",pos_black_pawn2)
 
 #黑卒3
-pos_black_pawn3 = list_chess[3][4]
+pos_black_pawn3 = list_position[3][4]
 black_black_pawn3 = Chess(root,"卒","black",pos_black_pawn3)
 
 #黑卒4
-pos_black_pawn4 = list_chess[3][6]
+pos_black_pawn4 = list_position[3][6]
 black_black_pawn4 = Chess(root,"卒","black",pos_black_pawn4)
 
 #黑卒5
-pos_black_pawn5 = list_chess[3][8]
+pos_black_pawn5 = list_position[3][8]
 black_black_pawn5 = Chess(root,"卒","black",pos_black_pawn5)
 
 
 #******************************************
 #红兵１
-pos_red_pawn1 = list_chess[6][0]
+pos_red_pawn1 = list_position[6][0]
 black_red_pawn1 = Chess(root,"兵","red",pos_red_pawn1)
 
 #红兵2
-pos_red_pawn2 = list_chess[6][2]
+pos_red_pawn2 = list_position[6][2]
 black_red_pawn2 = Chess(root,"兵","red",pos_red_pawn2)
 
 #红兵3
-pos_red_pawn3 = list_chess[6][4]
+pos_red_pawn3 = list_position[6][4]
 black_red_pawn3 = Chess(root,"兵","red",pos_red_pawn3)
 
 #红兵4
-pos_red_pawn4 = list_chess[6][6]
+pos_red_pawn4 = list_position[6][6]
 black_red_pawn4 = Chess(root,"兵","red",pos_red_pawn4)
 
 #红兵5
-pos_red_pawn5 = list_chess[6][8]
+pos_red_pawn5 = list_position[6][8]
 black_red_pawn5 = Chess(root,"兵","red",pos_red_pawn5)
 
 #左红炮
-pos_red_left_gun = list_chess[7][1]
+pos_red_left_gun = list_position[7][1]
 black_red_left_gun = Chess(root,"炮","red",pos_red_left_gun)
 
 #右红炮
-pos_red_right_gun = list_chess[7][7]
+pos_red_right_gun = list_position[7][7]
 black_red_right_gun = Chess(root,"炮","red",pos_red_right_gun)
 
 #左红车
-pos_red_left_rook = list_chess[9][0]
+pos_red_left_rook = list_position[9][0]
 red_left_rook = Chess(root,"車","red",pos_red_left_rook)
 
 #左红马
-pos_red_left_knight = list_chess[9][1]
+pos_red_left_knight = list_position[9][1]
 red_left_knight = Chess(root,"馬","red",pos_red_left_knight)
 
 #左红相
-pos_red_left_minister = list_chess[9][2]
+pos_red_left_minister = list_position[9][2]
 red_left_minister = Chess(root,"相","red",pos_red_left_minister)
 
 #左红仕
-pos_red_left_guard = list_chess[9][3]
+pos_red_left_guard = list_position[9][3]
 red_left_guard = Chess(root,"仕","red",pos_red_left_guard)
 
 #红帅
-pos_red_king = list_chess[9][4]
+pos_red_king = list_position[9][4]
 red_king = Chess(root,"帅","red",pos_red_king)
 
 #右红仕
-pos_red_right_guard = list_chess[9][5]
+pos_red_right_guard = list_position[9][5]
 red_right_guard = Chess(root,"仕","red",pos_red_right_guard)
 
 #右红相
-pos_red_right_minister = list_chess[9][6]
+pos_red_right_minister = list_position[9][6]
 red_right_minister = Chess(root,"相","red",pos_red_right_minister)
 
 #右红马
-pos_red_right_knight = list_chess[9][7]
+pos_red_right_knight = list_position[9][7]
 red_right_knight = Chess(root,"馬","red",pos_red_right_knight)
 
 #右红车
-pos_red_right_rook = list_chess[9][8]
+pos_red_right_rook = list_position[9][8]
 red_right_rook = Chess(root,"車","red",pos_red_right_rook)
 
 
