80074508e085a06515178a8039f5fda682ec7e2c
Tedu <lvze@tedu.cn>
2019-05-10 19:34:03 +0800

    提交

--- chessPieces.py
+++ chessPieces.py
@@ -1,6 +1,90 @@
 from chess import *
 
 class Rook(Chess):
+
+    def collect_move_positions(self,can_move,vector):
+
+        vector_list = []
+
+        for item in list_chess:
+            if vector == "up":
+                if item.pos_in_list[1] == self.pos_in_list[1] and item.pos_in_list[0] < self.pos_in_list[0]:
+                    vector_list.append(item)
+            elif vector == "down":
+                if item.pos_in_list[1] == self.pos_in_list[1] and item.pos_in_list[0] > self.pos_in_list[0]:
+                    vector_list.append(item)
+            elif vector == "left":
+                if item.pos_in_list[0] == self.pos_in_list[0] and item.pos_in_list[1] < self.pos_in_list[1]:
+                    vector_list.append(item)
+            elif vector == "right":
+                if item.pos_in_list[0] == self.pos_in_list[0] and item.pos_in_list[1] > self.pos_in_list[1]:
+                    vector_list.append(item)
+
+        #找边界子
+        bond_chess = None
+        for item in vector_list:
+            if not bond_chess:
+                bond_chess = item
+            else:
+                if vector == "up":
+                    if item.pos_in_list[0] > bond_chess.pos_in_list[0]:
+                        bond_chess = item
+                elif vector == "down":
+                    if item.pos_in_list[0] < bond_chess.pos_in_list[0]:
+                        bond_chess = item
+                elif vector == "left":
+                    if item.pos_in_list[1] > bond_chess.pos_in_list[1]:
+                        bond_chess = item
+                elif vector == "right":
+                    if item.pos_in_list[1] < bond_chess.pos_in_list[1]:
+                        bond_chess = item
+
+        #界限
+        position_bound = None
+        if vector == "up":
+            if bond_chess:
+                position_bound = bond_chess.pos_in_list[0]
+            else:
+                position_bound = 0
+        elif vector == "down":
+            if bond_chess:
+                position_bound = bond_chess.pos_in_list[0]
+            else:
+                position_bound = 9
+
+        elif vector == "left":
+            if bond_chess:
+                position_bound = bond_chess.pos_in_list[1]
+            else:
+                position_bound = 0
+        elif vector == "right":
+            if bond_chess:
+                position_bound = bond_chess.pos_in_list[1]
+            else:
+                position_bound = 8
+
+
+        # 添加中间元素包含边界子的列表
+        if vector == "up":
+            for c in range(self.pos_in_list[0] - 1, position_bound - 1, -1):
+                can_move.append(list_position[c][self.pos_in_list[1]])
+        elif vector == "down":
+            for c in range(self.pos_in_list[0]+1, position_bound +1):
+                can_move.append(list_position[c][self.pos_in_list[1]])
+        elif vector == "left":
+            for c in range(self.pos_in_list[1] - 1, position_bound - 1, -1):
+                can_move.append(list_position[self.pos_in_list[0]][c])
+        elif vector == "right":
+            for c in range(self.pos_in_list[1]+1, position_bound +1):
+                can_move.append(list_position[self.pos_in_list[0]][c])
+
+
+        # 如果边界与本子颜色相同从列表中去掉
+        if bond_chess and bond_chess.color == self.color:
+            can_move.remove(list_position[bond_chess.pos_in_list[0]][bond_chess.pos_in_list[1]])
+
+
+
     """
      车
     """
@@ -9,7 +93,15 @@ class Rook(Chess):
         获得该棋子可以移动的所有位置
         :return:
         """
-        pass
+        can_move = []
+
+        self.collect_move_positions(can_move,"up")
+        self.collect_move_positions(can_move, "down")
+        self.collect_move_positions(can_move, "left")
+        self.collect_move_positions(can_move, "right")
+
+        print(can_move)
+        return can_move
 
 class Knight(Chess):
     """
